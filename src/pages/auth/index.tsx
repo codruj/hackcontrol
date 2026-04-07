e250d1343681ddced33cc7aa612c14117246de49
codruj <codruta.jucan14@gmail.com>
2026-04-07 23:04:41 +0300

    modify callback url

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -14,7 +14,7 @@ const Auth = () => {
     setLoading(true);
     try {
       await signIn(provider, {
-        callbackUrl: `http://localhost:3000/app`,
+        callbackUrl: `/app`,
       });
     } catch (error) {
       toast.error("Unable to log in. Please try again later.");
